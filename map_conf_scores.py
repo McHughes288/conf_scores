79cf8e9e6368d98946c92c1f4a4d443c0e2fd9d6
johnh <johnh@speechmatics.com>
2020-12-05 20:42:19 +0000

    get loss, get grads, training loop, gradient ascent

--- map_conf_scores.py
+++ map_conf_scores.py
@@ -1,9 +1,12 @@
 from util import parse_sgml_file, parse_ctm_file, parse_word_string
 from absl import logging, flags, app
 import numpy as np
+from math import isnan
 
 flags.DEFINE_string("dev_set", None, "path to development SGML file used to calibrate conf scores")
 flags.DEFINE_string("test_set", None, "path to held-out test set to apply calibration to")
+flags.DEFINE_float("lr", 0.1, "path to development SGML file used to calibrate conf scores")
+flags.DEFINE_integer("steps", 1000, "path to held-out test set to apply calibration to")
 
 flags.mark_flag_as_required("dev_set")
 flags.mark_flag_as_required("test_set")
@@ -11,28 +14,60 @@ flags.mark_flag_as_required("test_set")
 FLAGS = flags.FLAGS
 
 
-def piecewise_linear_mapping(x, m1=0.55, m2=5, m3=0.55, c2=-2):
+def piecewise_linear_mapping(x, params):
+    m1, m2, m3, c2 = params
     a1 = c2 / (m1 - m2)
     a2 = (1 - m3 - c2) / (m2 - m3)
 
-    f = np.piecewise(
+    f1 = lambda x: m1 * x
+    f2 = lambda x: m2 * x + c2
+    f3 = lambda x: m3 * (x - 1) + 1
+
+    y = np.piecewise(
         x,
         [(0 <= x) * (x <= a1), (a1 < x) * (x < a2), (a2 <= x) * (x <= 1)],
-        [lambda x: m1 * x, lambda x: m2 * x + c2, lambda x: m3 * (x - 1) + 1],
+        [f1, f2, f3],
     )
-    return f
+    return y
 
 
-def get_loss(confs, labels, epsilon=1e-5):
-    print(len(confs), len(labels))
-    print(confs[0:20], labels[0:20])
-    mapped_confs = piecewise_linear_mapping(confs)
-    print(mapped_confs[0:20])
-
+def get_loss(confs, labels, params, epsilon=1e-3):
+    mapped_confs = piecewise_linear_mapping(confs, params)
     loss = labels * np.log(mapped_confs + epsilon) + (1 - labels) * np.log(
         1 - mapped_confs + epsilon
     )
     return loss.mean()
+
+
+def get_gradients(confs, labels, params):
+    m1, m2, m3, c2 = params
+    a1 = c2 / (m1 - m2)
+    a2 = (1 - m3 - c2) / (m2 - m3)
+
+    # param grads of first linear segment
+    mask = ((0 <= confs) * (confs <= a1)).astype(int)
+    y = labels * mask
+    x = confs * mask
+    grad_m1 = (y / m1) + (1 - y) * (-x / (1 - m1 * x))
+    grad_m1 = grad_m1.mean()
+
+    # param grads of second linear segment
+    mask = ((a1 < confs) * (confs < a2)).astype(int)
+    y = labels * mask
+    x = confs * mask
+    grad_c2 = (y / (m2 * x + c2)) + (1 - y) * (-1 / (1 - m2 * x - c2))
+    grad_c2 = grad_c2.mean()
+    grad_m2 = x * grad_c2
+    grad_m2 = grad_m2.mean()
+
+    # param grads of third linear segment
+    mask = ((a2 <= confs) * (confs <= 1)).astype(int)
+    y = labels * mask
+    x = confs * mask
+    grad_m3 = (y * (x - 1) / (m3 * (x - 1) + 1)) + (1 - y) / m3
+    grad_m3 = grad_m3.mean()
+
+    return np.array([grad_m1, grad_m2, grad_m3, grad_c2])
 
 
 def extract_train_samples(data):
@@ -54,17 +89,55 @@ def extract_train_samples(data):
 
 
 def main(unused_argv):
-
-    test_data = parse_ctm_file(FLAGS.test_set)
-    print(test_data[0:10])
-
+    # parse the dev data and extract confidence scores and labels
     dev_data = parse_sgml_file(FLAGS.dev_set)
-    print(dev_data[0:10])
-
     confs, labels = extract_train_samples(dev_data)
-    loss = get_loss(confs, labels)
 
-    print(loss)
+    # initialise params for piecewise linear mapping gradients m1, m2, m3 and intercept c2
+    params = np.array([0.55, 5, 0.55, -2])
+    prev_loss = -1000
+
+    # training loop with gradient ascent update
+    # result is a mapping that maximises confidence of correctly predicted words
+    # and minimises confidence of incorrectly predicted words
+    for step in range(FLAGS.steps):
+        loss = get_loss(confs, labels, params)
+        grads = get_gradients(confs, labels, params)
+
+        delta_loss = loss - prev_loss
+        prev_loss = loss
+        if step % 20 == 0:
+            print(
+                f"step {step}, loss {loss}, delta_loss {delta_loss}, params {params}, grads {grads}"
+            )
+
+        if isnan(loss):
+            print("Loss is nan so breaking training loop")
+            break
+
+        if np.abs(delta_loss) < 1e-10:
+            print(f"Change in loss is {delta_loss} < 1e-10, so breaking training loop")
+            break
+
+        # gradient ascent
+        params = params + FLAGS.lr * grads
+        # ensure gradient of lines are positive
+        m1, m2, m3, c2 = params
+        assert m1 > 0
+        assert m2 > 0
+        assert m3 > 0
+
+    print(f"final loss {loss}, final params {params}")
+
+    # Parse the test data, extract confidence scores and pass through the learnt linear mapping
+    test_data = parse_ctm_file(FLAGS.test_set)
+    test_confs = np.array([item["conf"] for item in test_data])
+    av_conf = test_confs.mean()
+
+    mapped_confs = piecewise_linear_mapping(test_confs, params)
+    av_mapped_conf = mapped_confs.mean()
+
+    print(f"av_conf {av_conf}, av_mapped_conf {av_mapped_conf}")
 
 
 if __name__ == "__main__":
