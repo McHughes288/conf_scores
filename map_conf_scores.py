05204e8489e4df6e8fbf52bdd38309744b38b508
johnh <johnh@speechmatics.com>
2020-12-05 16:56:52 +0000

    move relevant data parsing to util, convert times and conf to float

--- map_conf_scores.py
+++ map_conf_scores.py
@@ -13,32 +13,10 @@ FLAGS = flags.FLAGS
 def main(unused_argv):
 
     test_data = parse_ctm_file(FLAGS.test_set)
-    print(test_data[0:20])
+    print(test_data[0:10])
 
-    data = parse_sgml_file(FLAGS.dev_set)
-    relevant_data = []
-    for i, segment in enumerate(data("path")):
-        print(i, segment, segment.text)
-
-        text = segment.text.strip()
-
-        if text:
-            word_data = parse_word_string(text)
-            assert int(segment["word_cnt"]) == len(
-                word_data
-            ), f"Got {len(word_data)} words, expected {segment['word_cnt']}"
-
-            relevant_data.append(
-                {
-                    "file": segment["file"],
-                    "start_time": segment["r_t1"],
-                    "end_time": segment["r_t2"],
-                    "data": word_data,
-                }
-            )
-        if i > 20:
-            break
-    print(relevant_data)
+    dev_data = parse_sgml_file(FLAGS.dev_set)
+    print(dev_data[0:10])
 
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -4,7 +4,27 @@ from bs4 import BeautifulSoup
 def parse_sgml_file(file_path):
     with open(file_path, "r") as sgml_f:
         data = BeautifulSoup(sgml_f.read(), "html.parser")
-    return data
+
+    relevant_data = []
+    for i, segment in enumerate(data("path")):
+        text = segment.text.strip()
+
+        # add word information if present, otherwise continue
+        if text:
+            word_data = parse_word_string(text)
+            assert int(segment["word_cnt"]) == len(
+                word_data
+            ), f"Got {len(word_data)} words, expected {segment['word_cnt']}"
+
+            relevant_data.append(
+                {
+                    "file": segment["file"],
+                    "start": float(segment["r_t1"]),
+                    "end": float(segment["r_t2"]),
+                    "data": word_data,
+                }
+            )
+    return relevant_data
 
 
 def parse_word_string(text):
@@ -12,9 +32,14 @@ def parse_word_string(text):
     word_data = []
     for word in words:
         state, ref, pred, time_bracket, conf = word.split(",")
+        # There is no extra information if state is deletion
         if state != "D":
             start, end = time_bracket.split("+")
+            start = float(start)
+            end = float(end)
+            conf = float(conf)
             pred = pred.replace('"', "")
+
         else:
             pred, start, end, conf = None, None, None, None
         word_data.append(
@@ -42,10 +67,10 @@ def parse_ctm_file(file_path):
             data.append(
                 {
                     "file": file_name,
-                    "start": start,
-                    "end": end,
+                    "start": float(start),
+                    "end": float(end),
                     "word": word,
-                    "conf": conf,
+                    "conf": float(conf),
                 }
             )
     return data
